56209e0665a83956ae465074c33d01d2985e8f23
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-06-29 14:32:45 -0600

    making a new project

---
+++ src/CrearCuenta.java
@@ -0,0 +1,8 @@
+
+public class CrearCuenta {
+	public static void main(String[] args) {
+		Cuenta primerCuenta= new Cuenta();
+		primerCuenta.saldo=1000;
+		System.out.println(primerCuenta.saldo);
+	}
+}
