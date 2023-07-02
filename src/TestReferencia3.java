361b5afb137c046c2f4ace50e9dba1c1c1ac162e
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-01 22:00:22 -0600

    references between objects

--- src/TestReferencia3.java
+++ src/TestReferencia3.java
@@ -2,6 +2,10 @@
 public class TestReferencia3 {
 	public static void main(String[] args) {
 		Cuenta cuentaDiego=new Cuenta();
+		//this give us an error 
+		//System.out.println(cuentaDiego.titular.nombre);
+		Cliente cliente=new Cliente();
+		cuentaDiego.titular=cliente;
 		System.out.println(cuentaDiego.titular.nombre);
 	}
 }
