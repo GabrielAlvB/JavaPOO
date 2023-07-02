41e95847c71b938779c8fe70f22984bc38faf0f4
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-02 16:36:41 -0600

    new proyect, class Funcionario gets and setters

---
+++ proyect2ByteBankHeredado/src/TestFuncionarios.java
@@ -0,0 +1,10 @@
+
+public class TestFuncionarios {
+	public static void main(String[] args) {
+		Funcionario gabriel=new Funcionario();
+		gabriel.setNombre("Gabriel");
+		gabriel.setDocumento("165165");
+		gabriel.setSalario(22000);
+		System.out.println(gabriel.getSalario());
+	}
+}
