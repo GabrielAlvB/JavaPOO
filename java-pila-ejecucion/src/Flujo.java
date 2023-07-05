a594e777cca20295d2dbd74964ccc109b096d365
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-04 21:15:34 -0600

    working with exceptions

--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -1,4 +1,28 @@
-
 public class Flujo {
 
+  public static void main(String[] args) {
+    System.out.println("Ini do main");
+    metodo1();
+    System.out.println("Fim do main");
+  }
+  public static void metodo1(){
+    System.out.println("Ini do metodo1");
+    metodo2();
+    System.out.println("Fim do metodo1");
+  }
+  public static void metodo2(){
+    System.out.println("Ini do metodo2");
+    for(int i =1; i<= 5; i++){
+      System.out.println(i);
+      try {
+		int n=0;
+		int resultado=1/n;
+		System.out.println(resultado);
+      	} catch (ArithmeticException e) {
+		System.out.println(e.getMessage());
+		e.printStackTrace();
+		}
+    }
+    System.out.println("Fim do metodo2");
+  }
 }
