35416acf6b23247d9eedfe89f59b97626ed523d5
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-12 20:54:23 -0600

    Learning methods on Strings

--- byte-bank-libreria/src/com/bytebank/test/TestString.java
+++ byte-bank-libreria/src/com/bytebank/test/TestString.java
@@ -4,6 +4,12 @@ public class TestString {
 	public static void main(String[] args) {
 		String nombre= "Gabriel";
 		nombre=nombre.replace("a", "o");
+		nombre=nombre.toUpperCase();
+		char letra=nombre.charAt(0);
+		//nombre.charAt(1);
+		//nombre.indexOf("a");
 		System.out.println(nombre);
+		System.out.println(letra);
+	
 	}
 }
