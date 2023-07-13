1b2b713b8b86d9d8858b38f1c319965d0023b320
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-12 20:20:14 -0600

    learning how to use strings and their methods

---
+++ byte-bank-libreria/src/com/bytebank/test/TestString.java
@@ -0,0 +1,9 @@
+package com.bytebank.test;
+
+public class TestString {
+	public static void main(String[] args) {
+		String nombre= "Gabriel";
+		nombre=nombre.replace("a", "o");
+		System.out.println(nombre);
+	}
+}
