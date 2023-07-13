cce0237e2b10ba021055211b02fc2e70a36bb675
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-12 19:43:59 -0600

    exporting .jar and using it on testlibreria

---
+++ byte-bank-libreria/src/com/bytebank/test/TestLibreria.java
@@ -0,0 +1,11 @@
+package com.bytebank.test;
+
+import modelo.Cuenta;
+import modelo.CuentaAhorros;
+
+public class TestLibreria {
+	public static void main(String[] args) {
+		Cuenta cuenta= new CuentaAhorros(1,1);
+		cuenta.deposita(200);
+	}
+}
