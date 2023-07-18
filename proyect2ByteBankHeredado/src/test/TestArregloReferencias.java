d726bdf4ea49987a58a3ff6f672203028832655a
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-07-17 22:50:08 -0600

    Using an Array of diverses kind of objects that extends of them

--- proyect2ByteBankHeredado/src/test/TestArregloReferencias.java
+++ proyect2ByteBankHeredado/src/test/TestArregloReferencias.java
@@ -1,14 +1,19 @@
 package test;
 
+import modelo.Cuenta;
+import modelo.CuentaAhorros;
 import modelo.CuentaCorriente;
 
 public class TestArregloReferencias {
 	public static void main(String[] args) {
 		CuentaCorriente cc= new CuentaCorriente(1, 1);
 		//arreglo de objetos
-		CuentaCorriente[] cuentas= new CuentaCorriente[5];
+		Cuenta[] cuentas= new Cuenta[5];
 		cuentas[0]=cc;
-		
+		//podemos guardar en el array de tipo cuenta, diversas cuentas 
+		//que extiendan de cuenta
+		CuentaAhorros ca=new CuentaAhorros(23, 4);
+		cuentas[3]=ca;
 		System.out.println(cuentas[0]);
 		
 		cuentas[1]=new CuentaCorriente(2, 3);
